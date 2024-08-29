3870599e4929225c2b1aa2ce096aa7c7bded74e3
Michael Hosamani <michaelhosamani26@gmail.com>
2024-08-29 19:55:34 +0530

    Removed Unnecessary parts from the get() method

--- todos.js
+++ todos.js
@@ -6,15 +6,15 @@ app.use(express.json());
 let todos = [];
 
 app.get('/', (req, res)=>{
-    let todoArr = todos;
-    let todoObjects= [];
-    let numTodo = todoArr.length
-    for(let i=0; i<numTodo; i++){
-        todoObjects.push(todoArr[i]);
-    }
+    // let todoArr = todos;
+    // let todoObjects= [];
+    // let numTodo = todoArr.length
+    // for(let i=0; i<numTodo; i++){
+    //     todoObjects.push(todoArr[i]);
+    // }
 
     res.json({
-        todoArr
+        todos
     })
 })
 
